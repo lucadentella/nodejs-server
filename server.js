e5e10817fa4937b1752ec611cee08090348b383b
lucadentella <luca@dentella.it>
2021-11-19 14:19:33 +0100

    Add request header debug mode

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, cluster: ' + process.env.CLUSTER_NAME);
+  res.write('Hello World, cluster: ' + process.env.CLUSTER_NAME + "\n");
+  if(process.env.PRINT_HEADERS == 'true') {
+	res.write("\nREQUEST HEADERS:\n");
+	for(var header in req.headers)	
+		res.write(header + ": " + req.headers[header] + "\n");
+  }
+  res.end();
 });
 
 server.listen(port, hostname, () => {
